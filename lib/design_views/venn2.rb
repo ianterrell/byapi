3d42d3d2a0a382395bec7cd6ef096a58e3c72da3
Ian Terrell <ian.terrell@gmail.com>
2010-10-27 11:38:57 -0700

    added arrow/label style support and shrinking circles to venn2

--- lib/design_views/venn2.rb
+++ lib/design_views/venn2.rb
@@ -4,7 +4,7 @@ module DesignViews
       width = options[:width] || 100
       height = options[:height] || 100
       offsets = ::DesignViews::Helpers::Offsets.new(options[:offsets], width)
-      circle_offset = (5.to_f/300*height).to_i
+      circle_offset = (parameters[:shrink_circles].to_f/300*height).to_i
       first_center =  { :x => width/3 + circle_offset,   :y => width/3 - 2*circle_offset   }
       second_center = { :x => width/3*2 - circle_offset, :y => width/3*2 - 4*circle_offset}
       radius = width/3 - circle_offset*2
@@ -32,10 +32,15 @@ module DesignViews
         svg.lines_of_text parameters[:set_b_title], font_options.merge(:x => second_center[:x] + offsets.x("set_b"), :y => second_center[:y]+font_size/2+offsets.y("set_b"))
         
         # Intersection
+        parameters[:intersection_title] = "A: " + parameters[:intersection_title] if parameters[:style] == "label"
         intersection_title = ::DesignViews::Helpers::String.new parameters[:intersection_title], :font_size => font_size
         svg.lines_of_text intersection_title, font_options.merge(:x => height/2 + offsets.x("intersection"), :y => height-height/30 + offsets.y("intersection"))
-        svg.arrow(Vector[(width - intersection_title.max_width*0.80)/2 + offsets.x("arrow"), height-intersection_title.total_height-circle_offset*1.5 + offsets.y("arrow")], Vector[(second_center[:x]-first_center[:x])/2+first_center[:x], (second_center[:y]-first_center[:y])/2+first_center[:y]], :curve_to => Vector[height/6, height/3*2], :stroke => font_color, :"stroke-width" => arrow_size, :fill => "none")
-
+        if parameters[:style] == "label"
+          svg.lines_of_text "A", font_options.merge(:x => (second_center[:x]-first_center[:x])/2+first_center[:x], :y => (second_center[:y]-first_center[:y])/2+first_center[:y]+font_size.to_f/2)
+        else
+          svg.arrow(Vector[(width - intersection_title.max_width*0.80)/2 + offsets.x("arrow"), height-intersection_title.total_height-circle_offset*1.5 + offsets.y("arrow")], Vector[(second_center[:x]-first_center[:x])/2+first_center[:x], (second_center[:y]-first_center[:y])/2+first_center[:y]], :curve_to => Vector[height/6, height/3*2], :stroke => font_color, :"stroke-width" => arrow_size, :fill => "none")
+        end
+        
         svg.preview if options[:preview]
       end
     end
