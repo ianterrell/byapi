e02e0817f3822b0382db272d26886f125b8df1ce
Ian Terrell <ian.terrell@gmail.com>
2010-10-26 21:24:29 -0700

    added better feedback during approval process

--- app/controllers/admin/designs_controller.rb
+++ app/controllers/admin/designs_controller.rb
@@ -4,19 +4,23 @@ class Admin::DesignsController < Admin::BaseController
   
   def unapproved
     @site = Site.find params[:site_id]
+    approved_count = 0
+    rejected_count = 0
     if request.post?
       (params[:designs] || {}).each_pair do |id, values|
         design = Design.find id
         if values[:approval] == "1"
+          approved_count += 1
           design.tag_list = values[:tag_list]
           design.category_id = values[:category_id]
           design.save
           design.approve!
         elsif values[:approval] == "0"
+          rejected_count += 1
           design.destroy
         end
       end  
-      flash[:notice] = "Did stuff!"
+      flash[:notice] = "Approved #{approved_count} design#{'s' unless approved_count == 1}, rejected #{rejected_count}."
       redirect_to unapproved_admin_site_designs_path(@site)
     else
       @designs = @site.designs.unapproved
