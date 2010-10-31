6d2afeeeae55c11a9c757522bb184e78d2fd635f
Ian Terrell <ian.terrell@gmail.com>
2010-10-30 17:10:08 -0700

    fixed migration typo

--- db/migrate/20101031000339_remove_padding_fields.rb
+++ db/migrate/20101031000339_remove_padding_fields.rb
@@ -3,11 +3,11 @@ class RemovePaddingFields < ActiveRecord::Migration
     remove_column :designs, :cafepress_id_padded_x
     remove_column :designs, :cafepress_id_padded_y
     remove_column :designs, :cafepress_id_padded_y_big
-    remove_column :patterns, :padding
+    remove_column :products, :padding
   end
 
   def self.down
-    add_column :patterns, :padding, :string
+    add_column :products, :padding, :string
     add_column :designs, :cafepress_id_padded_y_big, :string
     add_column :designs, :cafepress_id_padded_y, :string
     add_column :designs, :cafepress_id_padded_x, :string
